c0994acd87804a44f6a6037fd00500565df2c4f9
Daniel Manrique <daniel.manrique@fly.io>
2023-08-23 16:55:23 -0400

    Add health check endpoint and use in fly.toml

--- app/main.py
+++ app/main.py
@@ -7,3 +7,10 @@ app = FastAPI()
 async def root():
     return {"message": "Hello Brave New World"}
 
+
+@app.get("/check/{check_id}")
+async def health_check(check_id: str):
+    import datetime
+    time = datetime.datetime.now()
+    return f"App is healthy: {time}"
+
