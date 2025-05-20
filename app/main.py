9d4d54fe4cf8fb38142ee0c78af1aca80359e70d
Daniel Manrique <daniel.manrique@fly.io>
2025-05-20 10:32:41 -0400

    nothing

--- app/main.py
+++ app/main.py
@@ -35,7 +35,7 @@ rce = Counter("redis_connection_errors_count", "Redis connection errors")
 
 @app.get("/")
 async def root():
-    return {"message": "Hello Brave New World by Aldous Huxley"}
+    return {"message": "Hello Brave New World! "}
 
 
 async def get_redis():
