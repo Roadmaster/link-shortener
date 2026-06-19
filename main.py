769d3a3191ad0473ac76b99e381e54631d4cc162
Daniel Manrique <daniel.manrique@fly.io>
2026-06-19 16:45:34 -0400

    yeh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def main():
     print("Hello from link-shortener! I do nothing. Just change every few mins to keep shithub happy.")
-
+    print("because yeh")
 
 if __name__ == "__main__":
     main()
